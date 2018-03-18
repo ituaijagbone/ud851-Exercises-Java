cabc04308eb4e160f5afdffbd383d22d18142d27
itua <itua.ijagbone@morningstar.com>
2018-03-17 23:09:23 -0500

    completed S09.02

--- Lesson09-ToDo-List/T09.02-Exercise-AddURIsToContract/app/src/main/java/com/example/android/todolist/data/TaskContract.java
+++ Lesson09-ToDo-List/T09.02-Exercise-AddURIsToContract/app/src/main/java/com/example/android/todolist/data/TaskContract.java
@@ -16,6 +16,7 @@
 
 package com.example.android.todolist.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 
@@ -29,11 +30,17 @@ public class TaskContract {
         3) Path(s) to the tasks directory
         4) Content URI for data in the TaskEntry class
       */
+    public static final String CONTENT_AUTHORITY = "com.example.android.todolist";
 
+    public static final Uri BASE_CONTENT = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String TASK_PATH = "tasks";
 
     /* TaskEntry is an inner class that defines the contents of the task table */
     public static final class TaskEntry implements BaseColumns {
 
+        public static final Uri CONTENT_URI = BASE_CONTENT
+                .buildUpon().appendPath(TASK_PATH).build();
 
         // Task table and column names
         public static final String TABLE_NAME = "tasks";
